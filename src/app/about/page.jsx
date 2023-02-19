e372c24725bf1819ffbbce622f798945f7dffb44
Alison Butcher <dev@alisonbutcher.com>
2023-02-19 10:19:56 +1000

    Added about page and styling

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs.
+      </p>
+      <p>
+        Our website is designed to provide you with a comprehensive database of
+        movies from around the world, along with the latest news, reviews and
+        trailers. Our movie database is constantly updated with new releases,
+        ensuring that you have access to the latest and greatest in the world of
+        movies. You can search for movies by title, director, actor, genre, or
+        release date making it easy to find the perfect movie for any mood.
+      </p>
+      <p>
+        In addition to our extensive movie database we also offer a platform for
+        movie lovers to connect and share their thoughts on the latest releases.
+        Our community section includes a forum where you can discuss your
+        favourite films with like-minded people and read reviews and ratings
+        from other users. We also have a section dedicated to movie news and
+        trailer to keep you updated with the latest happenings in this space.
+      </p>
+    </div>
+  );
+}
